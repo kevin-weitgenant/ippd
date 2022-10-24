b3eea9bed8d6e4ced4ee23ea25abd6e5277cddb0
kevin-weitgenant <kevin.weitgenant@gmail.com>
2022-10-24 16:58:33 -0300

    ler frames

--- ler_frames.cpp
+++ ler_frames.cpp
@@ -1,47 +1,46 @@
 #include<iostream>
 using namespace std;
 
-char *frameR;
-char *Lixo, *tabCb;
-char *frameA;
+char *frameR, *frameA, *Lixo;
 
-FILE *fr, *fw, *fwcb,*fwcr,*fw2;
+FILE *fr;
 
-void processFrames(int x,int y){
+void printFrame(char *frame,int x,int y){
+    char position;
+
+    for (int i =0; i<x; i++){
+        printf("\n");
+        for (int j =0; j<y; j++)
+            position = *(frame +(i*j + j)) ;
+            cout << (int)position;
+    }
+}
+
+void readFrames(int x,int y){
     
     frameR = (char*) malloc((size_t) (x * y));
-    Lixo = (char*) malloc((size_t) (x * y/2));
-
+    Lixo = (char*) malloc((size_t) (x * y/2));   // 2*(x/2,y/2) =  (x,y/2) 
     frameA = (char*) malloc((size_t) (x * y));
 
     fr = fopen("akiyo_qcif.yuv", "rb");//Input file
     
-    fw = fopen("frame1.yuv", "wb");
-    fwcb = fopen("framecb.yuv", "wb");
-    fw2 = fopen("frame2.yuv", "wb");
-
-    
-    for(int frame = 1; frame <= 1; frame++){
+    for(int frame = 1; frame <= 2; frame++){
         fread(frameR, (size_t) (x), (size_t) (y), fr);
         fread(Lixo, (size_t) (x), (size_t) (y/2), fr);
         fread(frameA, (size_t) (x), (size_t) (y), fr);
-        
-
-        fwrite(frameR, (size_t) (x * y), 1, fw);
-        fwrite(Lixo, (size_t) (x * y/2), 1, fwcb);
-        fwrite(frameA, (size_t) (x * y), 1, fw2);
+        fread(Lixo, (size_t) (x), (size_t) (y/2), fr);
     }
+
+    printf("Primeiro frame:\n\n\n\n");
+    printFrame(frameR, x, y);
+    printf("\n\nSegundo frame:\n\n\n\n");
+    printFrame(frameA, x, y);
 }
 
 int main(int argc, char *argv[]){
     
-    processFrames(176,144);
+    readFrames(176,144);
     //Closing the files
     fclose(fr);
-    fclose(fw);
-    fclose(fwcb);
-    fclose(fw2);
-
-
     return 0;
 }
